20b459bf807148ccf99db166bd28d2cb3c361d11
cindy kivala <cindykivala@gmail.com>
2025-08-08 15:19:52 +0300

    props and attr

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,16 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Nana", breed="Beagle"):       
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
+        
+Nana = Dog("Nana", "Beagle")
+print(f"{Nana.name} is a {Nana.breed}.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,18 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Cindy", job="Sales"):
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            self.name = None
+        else:
+            self.name = name.title()
+
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
+            self.job = None
+
+guido = Person(name="guido van rossum", job="Sales")
+print(guido.name)
